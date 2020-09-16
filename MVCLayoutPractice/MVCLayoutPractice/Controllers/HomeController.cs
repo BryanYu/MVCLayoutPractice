e3e96dab7ac2e1bd9b581d8cad6b36807e237ce0
Bryan.Yu <bryan.yu@johnsontechinc.com>
2020-09-16 22:14:19 +0800

    Layout

---
+++ MVCLayoutPractice/MVCLayoutPractice/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLayoutPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Buttons()
+        {
+            return View();
+        }
+
+        public ActionResult Cards()
+        {
+            return View();
+        }
+
+        public ActionResult Color()
+        {
+            return View();
+        }
+
+        public ActionResult Border()
+        {
+            return View();
+        }
+
+        public ActionResult Animation()
+        {
+            return View();
+        }
+
+        public ActionResult Other()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult Blank()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MVCLayoutPractice
+# MVCLayoutPractice 
+
+使用 [SB Admin 2](https://startbootstrap.com/themes/sb-admin-2/) 搭配ASP.NET MVC 
